7155814aca8974bef58cea5f92f11a02f53c29fe
rymanec123 <rymanec123@gmail.com>
2020-05-09 15:39:24 +0300

    Fixed main & style

--- HomeWork3/js/main.js
+++ HomeWork3/js/main.js
@@ -1,41 +1,36 @@
 var el = document.body;
-var btn = document.getElementsByClassName('l-tasks__btn_toggle')[0];
+var btn = document.getElementsByClassName('l-tasks__btn _toggle')[0];
+var btn2 = document.getElementsByClassName('l-tasks__btn _remove')[0];
+var span = document.getElementsByClassName('l-tasks__span');
+var itemText = document.getElementsByClassName('l-tasks__item-text')[0];
 
 var clickHandler = function(){
   el.classList.toggle('_color-schemie-light');
 }
 btn.addEventListener('click',clickHandler);
 
-var btn2 = document.getElementsByClassName('l-tasks__btn_remove')[0];
-var span = document.getElementsByClassName('l-tasks__span');
-var removeSpan = function(){
 
-     if(document.body.classList.contains('_color-schemie-light')){
+var removeSpan = function() {
 
-      span[9].remove('l-tasks__span'); 
-  
-     }
-     else{
-      span[0].remove('l-tasks__span');
-     }
-     btn2.removeEventListener('click',removeSpan);
+  if(el.classList.contains('_color-schemie-light')) {
+  span[9].remove(); 
     }
-    btn2.addEventListener('click',removeSpan);
-
-
-var result = function(num) {
-  
-  if( typeof num ==='number' && num % 2==0){
-    document.getElementsByClassName('l-tasks__item-text')[0].textContent ='This is a number: '+ num +' even';
+  else {
+  span[0].remove();
   }
-  else
-    if(typeof num ==='number' && num % 2!==0){
-     document.getElementsByClassName('l-tasks__item-text')[0].textContent ='This is a number: '+ num +' odd';
-  } 
-    else {
-     document.getElementsByClassName('l-tasks__item-text')[0].textContent ='This is not a number';
-   };
-   
-  };
-  result(4);
-  
+  btn2.removeEventListener('click',removeSpan);
+}
+btn2.addEventListener('click',removeSpan);
+
+
+(function(num) {
+  var type = ' odd';
+
+  if(typeof num !== "number") {
+    return itemText.textContent ='This is not a number';
+  }
+  else if(typeof num % 2 === 0) {
+    type = ' even';
+  }
+  itemText.textContent ='This is a number: '+ num + type;
+}(3));
