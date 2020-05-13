4073d2db82ac24d2d638bff75cd378786caf7666
rymanec123 <rymanec123@gmail.com>
2020-05-14 01:02:54 +0300

    qrySel - class

--- HomeWork4/main.js
+++ HomeWork4/main.js
@@ -1,31 +1,32 @@
-var earth = document.querySelector('img');
-var univers = document.querySelector('div');
+var earth = document.querySelector('.b-universe__img');
+var univers = document.querySelector('.b-universe');
 
 univers.addEventListener('click', function(e) {
-    var x = e.offsetX - earth.offsetWidth / 2 - 100;
-    var y = e.offsetY - earth.offsetHeight / 2 - 150;
+    var x = e.offsetX - earth.offsetWidth / 2;
+    var y = e.offsetY - earth.offsetHeight / 2;
     var target = e.target;
 
     if(earth.style.display !== 'none' && e.target.tagName === 'DIV') {
 
-        if(x < -100) {
-            x = -100;
+        if(x < 0) {
+            x = 0;
         }
-        else if(x > 600) {
-            x = 600;
+        else if(x > 700) {
+            x = 700;
         }
-        if(y < -150) {
-            y = -150;
+        if(y < 0) {
+            y = 0;
         }
-        else if(y > 300) {
-            y = 300;
+        else if(y > 450) {
+            y = 450;
         }
+        console.log(x, y)
 
         earth.style.transform ='translate(' + x + 'px , ' + y + 'px)';
     }
 
     if (target.tagName ==='IMG') {
-        earth.style.display = 'none';
+       return earth.style.display = 'none';
     }
     else {
         earth.style.display = '';
