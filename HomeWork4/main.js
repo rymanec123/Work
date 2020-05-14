baa7dc23bf20db17308c53a1714159a0c362377f
rymanec123 <rymanec123@gmail.com>
2020-05-14 15:32:31 +0300

    add var limitX & limitY

--- HomeWork4/main.js
+++ HomeWork4/main.js
@@ -1,5 +1,7 @@
 var imgEarth = document.querySelector('.b-universe__img');
 var univers = document.querySelector('.b-universe');
+var limitX = univers.offsetWidth - imgEarth.offsetWidth;
+var limitY = univers.offsetHeight - imgEarth.offsetHeight;
 
 univers.addEventListener('click', function(e) {
     var x = e.offsetX - imgEarth.offsetWidth / 2;
@@ -16,14 +18,14 @@ univers.addEventListener('click', function(e) {
     if(x < 0) {
         x = 0;
     }
-    else if(x > univers.offsetWidth - imgEarth.offsetWidth) {
-        x = univers.offsetWidth - imgEarth.offsetWidth;
+    else if(x > limitX) {
+        x = limitX;
     }
     if(y < 0) {
         y = 0;
     }
-    else if(y > univers.offsetHeight - imgEarth.offsetHeight) {
-        y = univers.offsetHeight - imgEarth.offsetHeight;
+    else if(y > limitY) {
+        y = limitY;
     }
 
     imgEarth.style.transform ='translate(' + x + 'px , ' + y + 'px)';
