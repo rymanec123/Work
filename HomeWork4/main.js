f2b51dd0efc9fb654afef05cbccc2ab42f67c323
rymanec123 <rymanec123@gmail.com>
2020-05-14 13:31:22 +0300

    fixed return

--- HomeWork4/main.js
+++ HomeWork4/main.js
@@ -1,35 +1,32 @@
-var earth = document.querySelector('.b-universe__img');
+var imgEarth = document.querySelector('.b-universe__img');
 var univers = document.querySelector('.b-universe');
 
 univers.addEventListener('click', function(e) {
-    var x = e.offsetX - earth.offsetWidth / 2;
-    var y = e.offsetY - earth.offsetHeight / 2;
+    var x = e.offsetX - imgEarth.offsetWidth / 2;
+    var y = e.offsetY - imgEarth.offsetHeight / 2;
     var target = e.target;
 
-    if(earth.style.display !== 'none' && e.target.tagName === 'DIV') {
-
-        if(x < 0) {
-            x = 0;
-        }
-        else if(x > 700) {
-            x = 700;
-        }
-        if(y < 0) {
-            y = 0;
-        }
-        else if(y > 450) {
-            y = 450;
-        }
-        console.log(x, y)
-
-        earth.style.transform ='translate(' + x + 'px , ' + y + 'px)';
+    if  (imgEarth.style.display === 'none') {
+        return imgEarth.style.display = '';
     }
+    if (target.tagName.toLowerCase() ==='img') {
+        return imgEarth.style.display = 'none';
+     }
 
-    if (target.tagName ==='IMG') {
-       return earth.style.display = 'none';
+    if(x < 0) {
+        x = 0;
     }
-    else {
-        earth.style.display = '';
+    else if(x > 700) {
+        x = 700;
     }
+    if(y < 0) {
+        y = 0;
+    }
+    else if(y > 450) {
+        y = 450;
+    }
+    console.log(x, y)
+
+    imgEarth.style.transform ='translate(' + x + 'px , ' + y + 'px)';
 });
 
