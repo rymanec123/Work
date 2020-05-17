eacd1f648e19a0e863ce2b3a442ba367e617991e
rymanec123 <rymanec123@gmail.com>
2020-05-17 18:01:29 +0300

    validator & stopPropagation

---
+++ HomeWork5/main.js
@@ -0,0 +1,34 @@
+var body = document.body;
+var form = document.querySelector('.form');
+var but = document.querySelector('button');
+var email = document.querySelector('input');
+
+form.addEventListener('submit', function(e) {
+    e.stopImmediatePropagation();
+    e.preventDefault();
+
+    var emailValue = e.target.elements.email.value;
+    var passValue = e.target.elements.password.value;
+
+    if(!emailValue || !passValue) {
+        alert('Поля не дожны быть пустыми!');
+    }
+    else if(passValue.length < 10) {
+        alert('Пароль не менее 10-ти символов')
+    }
+});
+
+but.addEventListener('click', function(e) {
+    e.stopImmediatePropagation();
+});
+
+body.addEventListener('click', function(e) {
+    e.stopImmediatePropagation();
+    console.log('body_1');
+});
+body.addEventListener('click', function(e) {
+    console.log('body_2');
+});
+body.addEventListener('click', function(e) {
+    console.log('body_3');
+});
