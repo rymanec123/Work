5917b7989cfe7f77e1df7606a3863ca30a93e09c
rymanec123 <rymanec123@gmail.com>
2020-05-20 13:07:23 +0300

    cycle; !colors.length; keyCode - else if

--- HomeWork6/main.js
+++ HomeWork6/main.js
@@ -2,7 +2,7 @@ var colors = [];
 var obj = {
     field1: 'test1',
     field2: 'test2',
-    field3: [],
+    field3: ['#00bcd4', ' #ffc107', '#2196f3'],
     field4: 'test4',
     field5: 'test5'
   };
@@ -10,32 +10,22 @@ var body = document.body;
 var buttonColor = document.querySelector('button');
 var counter;
 
-  for (var prop in obj) {
- 
-    if(Array.isArray(obj[prop])) {
-      obj[prop] = ['#00bcd4', ' #ffc107', '#2196f3']
-    }             
-  }
-
- buttonColor.addEventListener('click', function(e) {
-    var max = colors.length-1;
-    var min = 0;
-
+buttonColor.addEventListener('click', function(e) {
+   if (!colors.length) {
     for (var prop in obj) {
-   
-    if(body.style.backgroundColor === '' && Array.isArray(obj[prop])) {
-      colors = obj[prop];
+      if(Array.isArray(obj[prop])) {
+        colors = obj[prop];
+      }
     }
   }
-  counter = Math.floor(Math.random() * (max - min + 1)) + min;
 
+  counter = Math.floor(Math.random() * (colors.length));
   body.style.backgroundColor = colors[counter];
   });
 
   document.addEventListener('keydown', function(e) {
     if(e.keyCode === 39) {
-      
-        if (counter === colors.length-1) {
+      if (counter === colors.length-1) {
         counter = 0;
         }
         else if (counter >= 0) {
@@ -45,9 +35,8 @@ var counter;
             alert('Достаньте массив цветов!');
           }
     } 
-    if(e.keyCode === 37) {
-      
-        if(counter === 0) {
+     else if(e.keyCode === 37) {
+       if(counter === 0) {
           counter =  colors.length-1;
         }
         else if(counter <= colors.length-1) {
@@ -59,4 +48,3 @@ var counter;
       }
     body.style.backgroundColor = colors[counter];
   });
-  
