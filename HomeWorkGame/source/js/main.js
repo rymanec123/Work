2010bccf035f06c4ee154bdebfa30712af0e593d
rymanec123 <rymanec123@gmail.com>
2020-05-20 22:44:39 +0300

    level 1

---
+++ HomeWorkGame/source/js/main.js
@@ -0,0 +1,36 @@
+var aim = document.querySelector('.b-shooter__aim');
+var aimImg = document.querySelector('b-shooter__img-aim');
+var shooter = document.querySelector('.b-shooter');
+
+shooter.addEventListener('click', function(e) {
+    var x = e.offsetX - aim.offsetWidth / 2;
+    var y = e.offsetY - aim.offsetHeight / 2;
+    var limitX = shooter.offsetWidth - aim.offsetWidth;
+    var limitY = shooter.offsetHeight - aim.offsetHeight;
+
+    if(x < 0) {
+        x = 0;
+    }
+    else if(x > limitX) {
+        x = limitX;
+    }
+    if(y < 0) {
+        y = 0;
+    }
+    else if(y > limitY) {
+        y = limitY;
+    }
+    aim.style.left = x + 'px';
+    aim.style.top = y + 'px';
+});
+
+document.addEventListener('keydown', function(e) {
+    if(e.keyCode === 13) {
+        aim.style.padding ='10px';
+    }
+});
+document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 13) {
+        aim.style.padding ='';
+    }
+})
