7a31a6e42b3a349a705fd8e58a058ebb810a0ca2
rymanec123 <rymanec123@gmail.com>
2020-06-10 18:31:39 +0300

    Full

---
+++ HomeWork9/arr&STime.js
@@ -0,0 +1,62 @@
+'use strict'
+export {request, request2};
+function request(cb) {
+    const users = [
+      {
+        id: 1,
+        fistName: 'Chloe',
+        lastName: 'Snyder'
+      },  
+      {
+        id: 44,
+        fistName: 'Valdemar',
+        lastName: 'Larsen'
+      },
+      {
+        id: 342,
+        fistName: 'Curtis',
+        lastName: 'Garza'
+      },
+      {
+        id: 2,
+        fistName: 'Sedef',
+        lastName: 'Sezek'
+      },
+      {
+        id: 24,
+        fistName: 'Emile',
+        lastName: 'Taylor'
+      }
+    ];
+    setTimeout(function() {
+      cb(users);
+    }, 1000);
+  }
+function request2(cb2) {
+    const countries = [
+      {
+        userId: 44,
+        country: 'Germany'
+      },
+      {
+        userId: 1,
+        country: 'Canada'
+      },
+      {
+        userId: 342,
+        country: 'Brazil'
+      },
+      {
+        userId: 24,
+        country: 'Denmark'
+      },
+      {
+        userId: 2,
+        country: 'Ireland'
+      }
+    ];
+    setTimeout(function() {
+      cb2(countries);
+    }, 1000);
+  }
+  
---
+++ HomeWork9/main.js
@@ -0,0 +1,70 @@
+'use strict'
+import {request, request2} from './arr&STime.js';
+// Есть два массива: в первом содержится информация об имени пользователя, во втором - о стране.
+//  Сделайте два setTimeout, которые предоставляют эти массивы. Получите сначала массив users, затем массив countries.
+//   Когда оба массива получены, сопоставьте страну с пользователям с помощью id и создайте новый массив,
+//    который будет содержать объекты такого вида:
+//   {
+//     id: ...,
+//     fistName: ...,
+//     lastName: ...,
+//     country: ...
+//   }
+
+// Создайте список пользователей в html на основе данных из нового массива. 
+// В каждом элементе списка покажите firstName, lastName и country конкретного пользователя.
+
+// Разбейте получившийся js-код на два модуля.
+function createList() {
+  return document.createElement('ul');
+}
+function createListItem(text) {
+  const li = document.createElement('li');
+  li.innerText = text;
+  
+  return li;
+}
+request(function(x) {
+  request2(function(y) {
+    let userCountryArr = x.map(function(item) {
+      y.find(function(elem) {
+        if(elem.userId === item.id) {
+        item.country = elem.country
+        }
+      })
+      return item; 
+    });
+    function outeList(i, arg) {
+      const ul = createList();
+      const li = createListItem(`First Name: ${arg[i].fistName} 
+                                 Last Name: ${arg[i].lastName} 
+                                 Country: ${arg[i].country}`);
+       
+      ul.appendChild(li);
+      document.body.appendChild(ul);
+     }
+     outeList(0, userCountryArr);
+     outeList(1, userCountryArr);
+     outeList(2, userCountryArr);
+     outeList(3, userCountryArr);
+     outeList(4, userCountryArr);
+  });
+});
+
+// request(function(x) {
+//   request2(function(y) {
+//     let userCountryArr = x.reduce(function(acc, item) {
+//       for(let i=0; i < y.length; i++) {
+//         if(y[i].userId === item.id) {
+//           item.country = y[i].country;
+//           acc.push(item);
+//         }
+//       }
+//       return acc;
+//     }, []);
+//   });
+// });
+
+  
+  
+
