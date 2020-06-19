420f519a9bd91893e5fca5632f49daa3436475a1
rymanec123 <rymanec123@gmail.com>
2020-06-19 12:40:24 +0300

    fixed typeUsers & 2requestForUsers() ||

--- HomeWork10/main.js
+++ HomeWork10/main.js
@@ -1,43 +1,34 @@
 'use strict'
 
-const realRequest = function(seccess, seccess2, error) {
+const requestForUsers = function(seccess, error, typeUsers) {
     const xhr = new XMLHttpRequest();
-    const xhr2 = new XMLHttpRequest();
 
-    xhr2.open('GET', 'https://randomuser.me/api/?results=3&gender=female');
-
-    xhr2.onreadystatechange = function() {
-        if(xhr2.readyState === 4) {
-            if(xhr2.status >= 200 && xhr2.status < 300) {
-                seccess2(xhr2.response);
-            }
-            else {
-                error();
-            }
-        }
-    }
-    xhr2.send();
-
-    xhr.open('GET', 'https://randomuser.me/api/?results=3&gender=male');
+    xhr.open('GET', typeUsers);
 
     xhr.onreadystatechange = function() {
         if(xhr.readyState === 4) {
             if(xhr.status >= 200 && xhr.status < 300) {
-                seccess(xhr.response);
+                seccess(JSON.parse(xhr.response));
             }
             else {
-                error();
+                error({
+                    code: xhr.status,
+                    message: xhr.responseText
+                });
             }
         }
     }
     xhr.send();
 }
 
-realRequest(function(male) {
-    console.log(JSON.parse(male));
-}, function(female) {
-    console.log(JSON.parse(female));
-}, function(err) {
-    console.log('error')
-});
+const errorHandler = errorObj  => console.log(errorObj);
+
+requestForUsers(function(male) {
+    console.log(male);
+}, errorHandler, 'https://randomuser.me/api/?results=3&gender=male');
+
+requestForUsers(function(female) {
+    console.log(female);
+}, errorHandler, 'https://randomuser.me/api/?results=3&gender=female');
+
 
