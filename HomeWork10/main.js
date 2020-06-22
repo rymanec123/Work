ac006cdcc526041abfea3e2c6e0adfba77d63d7e
rymanec123 <rymanec123@gmail.com>
2020-06-22 18:53:31 +0300

    beautiful layout

--- HomeWork10/main.js
+++ HomeWork10/main.js
@@ -1,9 +1,8 @@
 'use strict'
-
 const requestForUsers = function(seccess, error, typeUsers) {
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', typeUsers);
+    xhr.open('GET', 'https://randomuser.me/api/?results=3' +typeUsers);
 
     xhr.onreadystatechange = function() {
         if(xhr.readyState === 4) {
@@ -21,14 +20,57 @@ const requestForUsers = function(seccess, error, typeUsers) {
     xhr.send();
 }
 
+const conteiner = document.querySelector('.conteiner')
+let arrMale;
+let arrFemale;
+
+function createList() {
+
+    return document.createElement('ul');
+}
+
 const errorHandler = errorObj  => console.log(errorObj);
+function arrUnion() {
+    if(Array.isArray(arrMale) && Array.isArray(arrFemale)) {
+        const arrUsers = [...arrMale, ...arrFemale];
+        const ul = createList();
+   
+        function createListItem(firstName, lastName, picture) {
+            const li = document.createElement('li');
+            const img = document.createElement('img');
+            const div = document.createElement('div');
+            const divText = document.createElement('div');
+
+            divText.innerText = `${firstName} ${lastName}`;
+
+            img.setAttribute('src', picture);
+            div.setAttribute('class', 'div_img');
+            divText.setAttribute('class', 'div_name');
+
+            div.appendChild(img)
+            li.appendChild(div);
+            li.appendChild(divText)
+
+            return li;
+        }
+     
+        for(let i = 0; i < arrUsers.length; i++) {
+          const li = createListItem(arrUsers[i].name.first, arrUsers[i].name.last, arrUsers[i].picture.large);
+
+          ul.appendChild(li); 
+        }
+        conteiner.appendChild(ul);
+    }
+}
 
 requestForUsers(function(male) {
-    console.log(male);
-}, errorHandler, 'https://randomuser.me/api/?results=3&gender=male');
+    arrMale = male.results;
+    arrUnion();
+}, errorHandler, '&gender=male');
 
 requestForUsers(function(female) {
-    console.log(female);
-}, errorHandler, 'https://randomuser.me/api/?results=3&gender=female');
+    arrFemale = female.results;
+    arrUnion();
+}, errorHandler, '&gender=female');
 
 
